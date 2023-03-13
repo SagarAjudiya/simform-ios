ba27989a79c999b793a2128631cedac45515b7f5
Sagar Ajudiya <sagarajudiya2002@gmail.com>
2023-03-13 10:00:17 +0530

    raw & associated

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -21,6 +21,12 @@ case .west :
     print("West")
 }
 
+enum DayInWeek: String {
+    case sunday, monday, tuesday, wednesday, thursday, friday, saturday
+}
+
+print(DayInWeek.sunday.rawValue)
+
 enum Beverage: CaseIterable {
     case coffee, tea, juice
 }
@@ -75,6 +81,79 @@ else {
 }
 
 
+// Raw value
+enum Size : Int {
+  case small = 10
+  case medium = 12
+  case large = 14
+}
+
+var result = Size.small.rawValue
+print(result)
+
+
+// Associated value
+enum Laptop {
+  case name(String)
+  case price(Int)
+}
+
+var brand = Laptop.name("Dell")
+print(brand)
+
+var offer = Laptop.price(1599)
+print(offer)
+
+
+enum Marks {
+  case gpa(Double, Double, Double)
+  case grade(String, String, String)
+}
+
+var marks1 = Marks.gpa(3.6, 2.9, 3.8)
+print(marks1)
+
+var marks2 = Marks.grade("A", "B", "C")
+print(marks2)
+
+
+// Names Associated Value
+enum Pizza {
+    case small (inches: Int)
+    case medium (inches: Int)
+    case large (inches: Int)
+}
+
+let pizza1 = Pizza.medium(inches: 12)
+print(pizza1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -106,25 +185,76 @@ arr1.append(Int(57.76))
 arr1.append(xyz())
 arr1.append(dictUser)
 
-for value in arr1 {
-    if let val = value as? [String : Any] {
-//        print(val)
-        if let val1 = val["history"] as? [Any] {
-            print(val1)
 
-            for i in val1 {
-                if let a = i as? Int {
-                    print("Int \(a)")
-                } else if let str = i as? String {
+for element in arr1 {
+    if let dict = element as? [String : Any] {
+        print(dict)
+        
+        if let valueOfkey = dict["history"] as? [Any?] {
+            print(valueOfkey)
+            
+            for index in valueOfkey {
+                if let num = index as? Int {
+                    print("Int \(num)")
+                } else if let str = index as? String {
                     print("String \(str)")
-                }else if let dub = i as? Double {
-                    print("Double \(dub)")
-                }else if let classs = i as? xyz {
-                    print("Class \(classs)")
+                } else if let dbl = index as? Double {
+                    print("Double \(dbl)")
                 }
             }
         }
     }
 }
+
+var str = "Sagar"
+
+var newStr = str.firstIndex(of: "g")
+
+if let newStr {
+    str.replaceSubrange(newStr...newStr, with: "h")
+//    str.replace("r", with: "d")
+}
+
+if let range = str.firstRange(of: "a") {
+    str.replacingCharacters(in: range, with: "d")
+}
+
+str.replacingOccurrences(of: "a", with: "z")
+
+
+let myNum: Int
+myNum = 1000
+print(myNum)
+
+
+
+
+var dict: [String : Any] = [:]
+dict["name"] = "Sagar"
+
+
+
+
+
+//for value in arr1 {
+//    if let val = value as? [String : Any] {
+////        print(val)
+//        if let val1 = val["history"] as? [Any] {
+//            print(val1)
+//
+//            for i in val1 {
+//                if let a = i as? Int {
+//                    print("Int \(a)")
+//                } else if let str = i as? String {
+//                    print("String \(str)")
+//                } else if let dub = i as? Double {
+//                    print("Double \(dub)")
+//                } else if let classs = i as? xyz {
+//                    print("Class \(classs)")
+//                }
+//            }
+//        }
+//    }
+//}
 
  
